8a1efdb0330904426a4d93d0473bf26e9143b2cd
wwwusonggg <wwwusonggg@gmail.com>
2020-11-28 21:30:07 +0800

    this commit is used sourcetree

--- Feedback.js
+++ Feedback.js
@@ -12,7 +12,8 @@ import FeedbackList from './FeedbackList';
 
 class Feedback extends React.Component{
   UNSAFE_componentWillMount(){
-    console.log('原来在2018年就在使用unsafe了')
+    console.log('原来在2018年就在使用unsafe了');
+    console.log('现在我使用sourcetree提交了');
     const {feedbackArr, dispatch} = this.props;
     if(!feedbackArr) {
       global.myFetch({url: global.serverBaseUrl+'/Myusers/me/feedbacks'})
