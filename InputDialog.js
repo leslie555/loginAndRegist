f132bc8fe77c6d69c0ec64c999f8921835597b59
leslie57 <wwwusonggg@gmail.com>
2021-04-09 11:19:38 +0800

    test-rm

--- InputDialog.js
+++ InputDialog.js
@@ -48,6 +48,7 @@ class InputDialog extends React.Component{
     console.log('c1111111');
     console.log('c222222222');
     console.log('c33333333333');
+    console.log('add');
     const newText = e.target.value;
     this.props.dispatch({
       type: 'inputDialogModel/updateState', 
